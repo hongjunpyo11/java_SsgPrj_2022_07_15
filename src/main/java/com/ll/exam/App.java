2eed7d4a636239cf20cfe58998510e7e879ebb74
hongjunpyo11 <jpyo98@naver.com>
2022-07-16 22:00:44 +0900

    명언등록시 번호증가 등록 완료

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -12,6 +12,8 @@ public class App {
     public void run() {
         System.out.println("== 명언 SSG ==");
 
+        int wiseSayingLastId = 0;
+
         outer:
         while (true) {
             System.out.print("명령) ");
@@ -19,10 +21,13 @@ public class App {
 
             switch (cmd) {
                 case "등록":
+                    int id = ++wiseSayingLastId;
                     System.out.println("명언 : ");
                     String content = sc.nextLine();
                     System.out.println("작가 : ");
                     String author = sc.nextLine();
+                    System.out.printf("%d번 명언이 등록되었습니다", id);
+                    break;
                 case "종료":
                     break outer;
             }
